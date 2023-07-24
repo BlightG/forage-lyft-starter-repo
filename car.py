8356b6e8ea9d47890ca63d7f49c455a263229e60
BlightG <birhane.gulilat@gmail.com>
2023-07-24 22:37:22 +0300

    refactored code to fit with new datastrucutre

--- car.py
+++ car.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
+from serviceable import Serviceable
+# from battery.battery import Battery
+# from engine.engine import Engine
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.__engine = engine
+        self.__battrey = battery
 
-    @abstractmethod
     def needs_service(self):
-        pass
+        if self.__engine.needs_service() or self.__battrey.needs_service():
+            return True
+        else:
+            return False
